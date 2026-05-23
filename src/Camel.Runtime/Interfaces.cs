83c96ba4c03ec252643de98ead5dfdb6f47fc7fe
Allister Beharry <allister.beharry@gmail.com>
2026-05-23 15:30:55 -0400

    Add Camel.Runtime project.

---
+++ src/Camel.Runtime/Interfaces.cs
@@ -0,0 +1,16 @@
+﻿namespace Camel;
+
+using System.Collections.Generic;   
+
+public interface IHtmlDisplay
+{
+    string Html();
+}
+
+public interface IPlugin
+{
+    string Name { get; }
+
+    Dictionary<string, Dictionary<string, object>> SharedState { get; }
+}
+
---
+++ src/Camel.Runtime/TestsRuntime.cs
@@ -0,0 +1,46 @@
+﻿namespace Camel;
+
+using System;
+
+using Microsoft.Extensions.Configuration;
+
+public class TestsRuntime : Runtime
+{
+    static TestsRuntime()
+    {
+        Runtime.WithFileAndConsoleLogging("Camel", "Tests", true);
+        config = LoadConfigFile("testappsettings.json");      
+        
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("Camel_API_TOKEN")))
+        {
+            Environment.SetEnvironmentVariable("Camel_API_TOKEN", config["ApiKey"], EnvironmentVariableTarget.Process);
+        }
+
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("GRADIENT_AGENT_API_TOKEN")))
+        {
+            Environment.SetEnvironmentVariable("GRADIENT_AGENT_API_TOKEN", config["ApiKey2"], EnvironmentVariableTarget.Process);
+        }
+
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("Camel_SPACES_ENDPOINT")))
+        {
+            Environment.SetEnvironmentVariable("Camel_SPACES_ENDPOINT", config["SpacesEndpoint"], EnvironmentVariableTarget.Process);
+        }
+
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("Camel_SPACES_ACCESS_KEY_ID")))
+        {
+            Environment.SetEnvironmentVariable("Camel_SPACES_ACCESS_KEY_ID", config["SpacesAccessKeyId"], EnvironmentVariableTarget.Process);
+        }
+
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("Camel_SPACES_ACCESS_KEY_SECRET")))
+        {
+            Environment.SetEnvironmentVariable("Camel_SPACES_ACCESS_KEY_SECRET", config["SpacesAccessKeySecret"], EnvironmentVariableTarget.Process);
+        }
+
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("Camel_SPACES_SESSION_BUCKET")))
+        {
+            Environment.SetEnvironmentVariable("Camel_SPACES_SESSION_BUCKET", config["SpacesSessionBucket"], EnvironmentVariableTarget.Process);
+        }
+    }    
+    static protected IConfigurationRoot config;
+}
+
